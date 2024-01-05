20f6b6d9862ff405b0866b8a711a9f15852b5f09
hyoloui <tmdgy9272@gmail.com>
2024-01-06 00:50:43 +0900

    init: directory setting
    - 폴더 구조를 설정합니다

---
+++ src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -0,0 +1,3 @@
+export default function UsernameStatusPage() {
+  return <div>UsernameStatusPage</div>;
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import { NextPage } from "next";
+
+const NotFound: NextPage = () => {
+  return (
+    <div>
+      <h1>이 페이지는 존재하지 않습니다. 다른 페이지를 검색해 보세요.</h1>
+      <Link href="/search">검색</Link>
+    </div>
+  );
+};
+
+export default NotFound;
