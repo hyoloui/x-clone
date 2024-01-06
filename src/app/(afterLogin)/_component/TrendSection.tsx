19cd800bd7d5708e5c63226d7241ef01621fc792
hyoloui <tmdgy9272@gmail.com>
2024-01-06 18:54:21 +0900

    refactor: /explore 경로 조건 추가
    - 탐색하기 에서는 TrendSection 이 렌더링 되지 않습니다

--- src/app/(afterLogin)/_component/TrendSection.tsx
+++ src/app/(afterLogin)/_component/TrendSection.tsx
@@ -1,7 +1,15 @@
+"use client";
+
 import style from "./trendSection.module.css";
+import { usePathname } from "next/navigation";
 import Trend from "./Trend";
 
 export default function TrendSection() {
+  const pathname = usePathname();
+
+  if (pathname === "/explore") {
+    return null;
+  }
   return (
     <div className={style.trendBg}>
       <div className={style.trend}>
