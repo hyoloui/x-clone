5fe37bdc6fff9132610ce854e0bbd7337a35c581
hyoloui <tmdgy9272@gmail.com>
2024-01-06 18:07:07 +0900

    style: Post 컴포넌트 제작 및 적용
    - 게시글을 보여주는 컴포넌트인 Post 컴포넌트를 제작하고 스타일링 합니다
    - /home 페이지 경로에 적용합니다

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,14 +1,19 @@
 import styles from "@/app/(afterLogin)/home/_component/tab.module.css";
-import TabProvider from "./_component/TabProvider";
-import Tab from "./_component/Tab";
-import PostForm from "./_component/PostForm";
+import Tab from "@/app/(afterLogin)/home/_component/Tab";
+import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
+import PostForm from "@/app/(afterLogin)/home/_component/PostForm";
+import Post from "@/app/(afterLogin)/_component/Post";
 
 export default function HomePage() {
+  const postList: number[] = Array.from({ length: 20 }, (_, i) => i);
   return (
     <main className={styles.main}>
       <TabProvider>
         <Tab />
         <PostForm />
+        {postList.map((index) => (
+          <Post key={index} />
+        ))}
       </TabProvider>
     </main>
   );
