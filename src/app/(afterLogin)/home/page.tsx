a44e38ce117f24e1db8ace1ef987332970b63bf4
hyoloui <tmdgy9272@gmail.com>
2024-01-06 17:41:03 +0900

    style: PostForm 생성 적용
    - PostForm 를 생성하고 스타일링해 /home page에 적용합니다

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,14 +1,14 @@
 import styles from "@/app/(afterLogin)/home/_component/tab.module.css";
 import TabProvider from "./_component/TabProvider";
 import Tab from "./_component/Tab";
+import PostForm from "./_component/PostForm";
 
 export default function HomePage() {
   return (
     <main className={styles.main}>
       <TabProvider>
-        home
         <Tab />
-        {/* <Post /> */}
+        <PostForm />
       </TabProvider>
     </main>
   );
