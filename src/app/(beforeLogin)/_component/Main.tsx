cc657101d07c910d76e4d96a8b4f128106a51966
hyoloui <tmdgy9272@gmail.com>
2024-01-06 15:50:23 +0900

    style: Main 컴포넌트 생성
    - Root Page에 적용될 Main 컴포넌트를 생성합니다
    - main.module.css 파일로 스타일링 합니다

---
+++ src/app/(beforeLogin)/_component/Main.tsx
@@ -0,0 +1,24 @@
+import styles from "@/app/(beforeLogin)/_component/main.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import zLogo from "@/../public/zlogo.png";
+
+export default function Main() {
+  return (
+    <>
+      <div className={styles.left}>
+        <Image src={zLogo} alt="logo" priority />
+      </div>
+      <div className={styles.right}>
+        <h1>지금 일어나고 있는 일</h1>
+        <h2>지금 가입하세요</h2>
+        <Link href="/i/flow/signup" className={styles.signup}>
+          계정 만들기
+        </Link>
+        <Link href="/login" className={styles.login}>
+          로그인
+        </Link>
+      </div>
+    </>
+  );
+}
