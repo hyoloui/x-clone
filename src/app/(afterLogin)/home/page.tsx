a7501225daa6bb175f41ab12c2791724b53deab9
hyoloui <tmdgy9272@gmail.com>
2024-01-06 17:29:34 +0900

    update: home 페이지 TabProvider 컴포넌트 적용
    - 미리 생성한 TabProvider 컴포넌트를 적용합니다

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,3 +1,15 @@
+import styles from "@/app/(afterLogin)/home/_component/tab.module.css";
+import TabProvider from "./_component/TabProvider";
+import Tab from "./_component/Tab";
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <main className={styles.main}>
+      <TabProvider>
+        home
+        <Tab />
+        {/* <Post /> */}
+      </TabProvider>
+    </main>
+  );
 }
