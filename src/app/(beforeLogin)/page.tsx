c5a6dff0c0a1eb49c916f7b0e04be2c32ce78093
hyoloui <tmdgy9272@gmail.com>
2024-01-06 15:57:01 +0900

    update: Main 컴포넌트 적용
    - Root Main 페이지를 /login 경로에서 보여주도록 합니다
    - 로그인 페이지에서 replace 를 활용해 'i/flow/login'으로 리다이렉팅을 수정합니다

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -1,24 +1,5 @@
-import styles from "@/app/page.module.css";
-import Link from "next/link";
-import Image from "next/image";
-import zLogo from "@/../public/zlogo.png";
+import Main from "@/app/(beforeLogin)/_component/Main";
 
-export default function Home() {
-  return (
-    <>
-      <div className={styles.left}>
-        <Image src={zLogo} alt="logo" priority />
-      </div>
-      <div className={styles.right}>
-        <h1>지금 일어나고 있는 일</h1>
-        <h2>지금 가입하세요</h2>
-        <Link href="/i/flow/signup" className={styles.signup}>
-          계정 만들기
-        </Link>
-        <Link href="/i/flow/login" className={styles.login}>
-          로그인
-        </Link>
-      </div>
-    </>
-  );
+export default async function Home() {
+  return <Main />;
 }
