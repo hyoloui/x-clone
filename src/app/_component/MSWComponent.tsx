63a91186e7025bb9ea074d11795c8eeb74b7f2ed
hyoloui <tmdgy9272@gmail.com>
2024-01-07 18:04:08 +0900

    fix: MSWComponent 에러 수정
    - 클라이언트에서 실행 되어야 하는 require 문의 조건이 맞지않아 localhost 런타임 에러 발생으로 수정합니다

--- src/app/_component/MSWComponent.tsx
+++ src/app/_component/MSWComponent.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 export default function MSWComponent() {
   useEffect(() => {
-    if (typeof window === "undefined") {
+    if (typeof window !== "undefined") {
       if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
         require("@/mocks/browser");
       }
