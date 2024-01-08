06fca0e4002d45a7ffd84b6adf3c89c95f759a48
hyoloui <tmdgy9272@gmail.com>
2024-01-09 01:41:00 +0900

    feature: home server component data fetch for react-query
    - Add React Query for prefetching data & use HydationBoundary

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -3,18 +3,44 @@ import Tab from "@/app/(afterLogin)/home/_component/Tab";
 import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
 import PostForm from "@/app/(afterLogin)/home/_component/PostForm";
 import Post from "@/app/(afterLogin)/_component/Post";
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
 
-export default function Home() {
+async function getPostsRecommends() {
+  const response = await fetch("http://localhost:9090/api/postRecommends", {
+    next: {
+      tags: ["posts", "recommends"],
+    },
+    cache: "no-store",
+  });
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response.json();
+}
+export default async function Home() {
   const postList: number[] = Array.from({ length: 20 }, (_, i) => i);
+
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["posts", "recommends"],
+    queryFn: getPostsRecommends,
+  });
+  const dehydratedState = dehydrate(queryClient);
   return (
     <main className={styles.main}>
-      <TabProvider>
-        <Tab />
-        <PostForm />
-        {postList.map((index) => (
-          <Post key={index} />
-        ))}
-      </TabProvider>
+      <HydrationBoundary state={dehydratedState}>
+        <TabProvider>
+          <Tab />
+          <PostForm />
+          {postList.map((index) => (
+            <Post key={index} />
+          ))}
+        </TabProvider>
+      </HydrationBoundary>
     </main>
   );
 }
