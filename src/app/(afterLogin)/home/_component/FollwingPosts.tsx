7435f9597beb944778f003ba871452b74c6eb4cf
hyoloui <tmdgy9272@gmail.com>
2024-01-13 22:20:57 +0900

    api: 팔로우 중 상태의 api 호출, getFollowingPosts
    - 팔로우중인 게시글을 가져오기 위한 api 호출과, data를 보여주는 컴포넌트를 생성합니다

---
+++ src/app/(afterLogin)/home/_component/FollwingPosts.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { getFollwingPosts } from "../_lib/getFollowingPosts";
+import Post from "../../_component/Post";
+
+export default function FollwingPosts() {
+  const { data } = useQuery<Post[]>({
+    queryKey: ["posts", "followings"],
+    queryFn: getFollwingPosts,
+    staleTime: 60 * 1000, // 1분
+    gcTime: 60 * 1000 * 5, // 5분
+  });
+
+  return data?.map((post) => <Post key={post.postId} post={post} />);
+}
