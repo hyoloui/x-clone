5ea764c74e16bc45c53d46bb4d1d8ce644fe9f15
hyoloui <tmdgy9272@gmail.com>
2024-01-14 23:34:59 +0900

    feature: useInfinityQuery 작성
    - 무한 스크롤 기능을 적용하기 위해 prefetchInfiniteQuery, useInfinityQuery 로 수정합니다

--- src/app/(afterLogin)/home/_component/PostRecommends.tsx
+++ src/app/(afterLogin)/home/_component/PostRecommends.tsx
@@ -1,16 +1,31 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { InfiniteData, useInfiniteQuery } from "@tanstack/react-query";
 import Post from "@/app/(afterLogin)/_component/Post";
 import { getPostRecommends } from "../_lib/getPostRecommends";
+import { Fragment } from "react";
 
 export default function PostRecommends() {
-  const { data } = useQuery<Post[]>({
+  const { data } = useInfiniteQuery<
+    Post[],
+    Object,
+    InfiniteData<Post[]>,
+    [_1: string, _2: string],
+    number
+  >({
     queryKey: ["posts", "recommends"],
     queryFn: getPostRecommends,
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => lastPage.at(-1)?.postId,
     staleTime: 60 * 1000, // 1분
     gcTime: 60 * 1000 * 5, // 5분
   });
 
-  return data?.map((post) => <Post key={post.postId} post={post} />);
+  return data?.pages.map((page, i) => (
+    <Fragment key={i}>
+      {page.map((post) => (
+        <Post key={post.postId} post={post} />
+      ))}
+    </Fragment>
+  ));
 }
--- src/app/(afterLogin)/home/_lib/getPostRecommends.ts
+++ src/app/(afterLogin)/home/_lib/getPostRecommends.ts
@@ -1,10 +1,14 @@
-export async function getPostRecommends(): Promise<Post[]> {
-  const response = await fetch("http://localhost:9090/api/postRecommends", {
-    next: {
-      tags: ["posts", "recommends"],
-    },
-    cache: "no-store",
-  });
+type Props = { pageParam?: number };
+export async function getPostRecommends({ pageParam }: Props): Promise<Post[]> {
+  const response = await fetch(
+    `http://localhost:9090/api/postRecommends?cursor=${pageParam}`,
+    {
+      next: {
+        tags: ["posts", "recommends"],
+      },
+      cache: "no-store",
+    }
+  );
   if (!response.ok) {
     throw new Error("Network response was not ok");
   }
