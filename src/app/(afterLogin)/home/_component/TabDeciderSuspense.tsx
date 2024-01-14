dbec538d8dac20e580f5e2596b574112ba7815c0
hyoloui <tmdgy9272@gmail.com>
2024-01-15 02:00:05 +0900

    update: home 페이지 suspense 적용
    - api 호출이 발생하는 부분인 TabDecider 컴포넌트를 분리합니다
    - Suspense 컴포넌트는 서버사이드 렌더링 시 스트리밍 방식으로 클라이언트에 데이터를 전송합니다

---
+++ src/app/(afterLogin)/home/_component/TabDeciderSuspense.tsx
@@ -0,0 +1,22 @@
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
+import TabDecider from "@/app/(afterLogin)/home/_component/TabDecider";
+import { getPostRecommends } from "@/app/(afterLogin)/home/_lib/getPostRecommends";
+
+export default async function TabDeciderSuspense() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: ["posts", "recommends"],
+    queryFn: getPostRecommends,
+    initialPageParam: 0,
+  });
+  const dehydratedState = dehydrate(queryClient);
+  return (
+    <HydrationBoundary state={dehydratedState}>
+      <TabDecider />
+    </HydrationBoundary>
+  );
+}
--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,33 +1,21 @@
-import styles from "@/app/(afterLogin)/home/_component/tab.module.css";
+import styles from "@/app/(afterLogin)/home/home.module.css";
+import { Suspense } from "react";
 import Tab from "@/app/(afterLogin)/home/_component/Tab";
 import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
 import PostForm from "@/app/(afterLogin)/home/_component/PostForm";
-
-import {
-  HydrationBoundary,
-  QueryClient,
-  dehydrate,
-} from "@tanstack/react-query";
-import { getPostRecommends } from "./_lib/getPostRecommends";
-import TabDecider from "./_component/TabDecider";
+import Loading from "@/app/(afterLogin)/home/loading";
+import TabDeciderSuspense from "@/app/(afterLogin)/home/_component/TabDeciderSuspense";
 
 export default async function Home() {
-  const queryClient = new QueryClient();
-  await queryClient.prefetchInfiniteQuery({
-    queryKey: ["posts", "recommends"],
-    queryFn: getPostRecommends,
-    initialPageParam: 0,
-  });
-  const dehydratedState = dehydrate(queryClient);
   return (
     <main className={styles.main}>
-      <HydrationBoundary state={dehydratedState}>
-        <TabProvider>
-          <Tab />
-          <PostForm />
-          <TabDecider />
-        </TabProvider>
-      </HydrationBoundary>
+      <TabProvider>
+        <Tab />
+        <PostForm />
+        <Suspense fallback={<Loading />}>
+          <TabDeciderSuspense />
+        </Suspense>
+      </TabProvider>
     </main>
   );
 }
