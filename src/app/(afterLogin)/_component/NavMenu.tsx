d2b6348ce0dc665b9d23ed1a11cca918f82fcaa3
hyoloui <tmdgy9272@gmail.com>
2024-01-14 22:54:01 +0900

    update: 내 정보 가져오기
    - 기존 더미데이터를 useSession을 활용해 session 데이터로 수정합니다

--- src/app/(afterLogin)/_component/NavMenu.tsx
+++ src/app/(afterLogin)/_component/NavMenu.tsx
@@ -3,14 +3,12 @@
 import style from "./navMenu.module.css";
 import { useSelectedLayoutSegment } from "next/navigation";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
 
 export default function NavMenu() {
   const segment = useSelectedLayoutSegment();
   console.log(segment);
-  const me = {
-    // 임시로 내 정보 있는것처럼
-    id: "hyoloui",
-  };
+  const { data: me } = useSession();
 
   return (
     <>
@@ -89,11 +87,11 @@ export default function NavMenu() {
           </div>
         </Link>
       </li>
-      {me?.id && (
+      {me?.user?.email && (
         <li>
-          <Link href={`/${me?.id}`}>
+          <Link href={`/${me?.user?.email}`}>
             <div className={style.navPill}>
-              {segment === me.id ? (
+              {segment === me?.user?.email ? (
                 <>
                   <svg width={26} viewBox="0 0 24 24" aria-hidden="true">
                     <g>
