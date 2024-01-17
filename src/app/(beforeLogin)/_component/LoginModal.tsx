e02b23d4839db324d6344506e3ee61f6402d3b8e
hyoloui <tmdgy9272@gmail.com>
2024-01-18 01:30:04 +0900

    update: 로그인 에러 핸들링
    - 로그인 실패 시 message를 보여줍니다

--- src/app/(beforeLogin)/_component/LoginModal.tsx
+++ src/app/(beforeLogin)/_component/LoginModal.tsx
@@ -16,12 +16,16 @@ export default function LoginModal() {
     e.preventDefault();
     setMessage("");
     try {
-      await signIn("credentials", {
+      const response = await signIn("credentials", {
         username: id,
         password,
         redirect: false,
       });
-      router.replace("/home"); // 베타 4버전 버그로 3버전 사용
+      if (response?.error) {
+        setMessage("아이디와 비밀번호가 일치하지 않습니다.");
+      } else {
+        router.replace("/home"); // 베타 4버전 버그로 3버전 사용
+      }
     } catch (err) {
       console.error(err);
       setMessage("아이디와 비밀번호가 일치하지 않습니다.");
