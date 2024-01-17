8615ee354c110b87c2f934a1bc2e4360878de4c8
hyoloui <tmdgy9272@gmail.com>
2024-01-18 02:24:17 +0900

    update: 이미지 경로 설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/upload/:path*",
+        destination: "http://localhost:9090/upload/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
