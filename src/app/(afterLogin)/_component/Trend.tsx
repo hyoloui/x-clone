2bd068737006b7efa01e7142e70fc9a53d6d8cde
hyoloui <tmdgy9272@gmail.com>
2024-01-31 02:00:17 +0900

    update: getSearchResult 함수 수정
    - getSearchResult: api 주소를 수정합니다
    - new URLSearchParams: 객체를 url 로 변환합니다
    - encodeURIComponent: Link 경로로 전달하는 title 값의 특수문자를 encoding 합니다

--- src/app/(afterLogin)/_component/Trend.tsx
+++ src/app/(afterLogin)/_component/Trend.tsx
@@ -5,7 +5,10 @@ import Link from "next/link";
 type Props = { trend: Hashtag };
 export default function Trend({ trend }: Props) {
   return (
-    <Link href={`/search?q=${trend.title}`} className={style.container}>
+    <Link
+      href={`/search?q=${encodeURIComponent(trend.title)}`}
+      className={style.container}
+    >
       <div className={style.count}>실시간 트렌드</div>
       <div className={style.title}>{trend.title}</div>
       <div className={style.count}>{trend.count.toLocaleString()} posts</div>
